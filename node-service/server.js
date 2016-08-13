6161844ddb009fca6f27634ad88831448a0eeff4
George Utsin <georgeutsin@gmail.com>
2016-08-13 14:56:31 -0400

    Added ability for angular app to access api

--- node-service/server.js
+++ node-service/server.js
@@ -18,6 +18,29 @@ module.exports.start = function (done) {
 
   connection.init();
 
+  app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    if (req.method == 'OPTIONS') {
+      res.sendStatus(200);
+    }else {
+      next();
+    }
+  });
+
   var router = express.Router();
   routes.configure(router);
   app.use('/v1', router);
